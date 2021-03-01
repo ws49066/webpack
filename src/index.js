c5394884109546cec1d8d6ef98b8f60f4f152479
ws49066 <ws49066@gmail.com>
2021-03-01 08:33:08 -0400

    Fim da configuraçao do WEBPACK SASS/CSS/JAVA/BOOTSTRAP/JQUERY

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/sass/index.scss'
+import './assets/css/estilo.css'
+import 'jquery'
+import 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPLugin = require('css-minimizer-webpack-plugin')
+
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -13,11 +16,34 @@ module.exports = {
         contentBase: './build',
         port: 9000
     },
+    // optimization:{
+    //     minimize:true,
+    //     minimizer: [new CssMinimizerPLugin({ test: /\.css$/})]
+    // },
     plugins: [
         new CopyWebpackPlugin({
             patterns: [
                 { from: '**/*.html',  context: 'src/' }
             ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
 }
